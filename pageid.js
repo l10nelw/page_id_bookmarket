42f4e9c1edbd480906b22e89333786d2ba50cb43
Lionel Wong <lionelster@gmail.com>
2017-01-12 14:22:32 +0800

    reddit fix, now works without being logged in

--- pageid.js
+++ pageid.js
@@ -1,5 +1,5 @@
 // without whitespace
-javascript:function getmeta(x){m=document.getElementsByTagName('meta');for(i=0;i<m.length;i++)if(m[i].getAttribute('property')==x)return m[i].getAttribute('content');return ''}function out(x)void prompt('',x);a=location.href.split(/[\/&=?#]+/);if(a[1]=='www.reddit.com'){if(a[7]){b=document.title.split(' ')[0];a[5]+=' '+a[7]}else{b=document.getElementsByClassName('tagline')[1];b=b.childNodes[5].innerHTML||b.childNodes[3].innerHTML}out(b+' '+a[5]+' ')}else if(a[1]=='www.youtube.com')out(document.getElementsByClassName('yt-user-info')[0].textContent.trim()+' '+a[a.indexOf('v')+1]+' '+document.getElementById('eow-title').title);else if(a[1].slice(-11)=='.tumblr.com')out(a[1]+' '+a[3]);else if(a[1]=='www.facebook.com')out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML+' '+a.slice(2,6).join(' '));else if(a[1].slice(-15)=='.deviantart.com')out(a.slice(1,4).join(' '));else if(a[1]=='instagram.com'&&a[2]=='p'){out(window._sharedData.entry_data.PostPage[0].media.owner.username+' '+a[3]);location.href=getmeta('og:video')||getmeta('og:image')}else void(0)
+javascript:function getmeta(x){m=document.getElementsByTagName('meta');for(i=0;i<m.length;i++)if(m[i].getAttribute('property')==x)return m[i].getAttribute('content');return '';}function out(x){void prompt('',x);}a=location.href.split(/[\/&=?#]+/);if(a[1]=='www.reddit.com'){if(a[7])a[5]+=' '+a[7];b=document.querySelectorAll('[data-cid]')[0].getAttribute('data-author');out(b+' '+a[5]+' ');}else if(a[1]=='www.youtube.com')out(document.getElementsByClassName('yt-user-info')[0].textContent.trim()+' '+a[a.indexOf('v')+1]+' '+document.getElementById('eow-title').title);else if(a[1].slice(-11)=='.tumblr.com')out(a[1]+' '+a[3]);else if(a[1]=='www.facebook.com')out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML+' '+a.slice(2,6).join(' '));else if(a[1].slice(-15)=='.deviantart.com')out(a.slice(1,4).join(' '));else if(a[1]=='instagram.com'&&a[2]=='p'){out(window._sharedData.entry_data.PostPage[0].media.owner.username+' '+a[3]);location.href=getmeta('og:video')||getmeta('og:image')}else void(0)
 
 // with whitespace
 javascript:
@@ -13,13 +13,9 @@ function getmeta(x){
 function out(x)void prompt('',x);
 a=location.href.split(/[\/&=?#]+/);
 if(a[1]=='www.reddit.com'){
-	if(a[7]){ // then it's a comment post
-		b=document.title.split(' ')[0];
-		a[5]+=' '+a[7]
-	}else{ // then it's an original post
-		b=document.getElementsByClassName('tagline')[1];
-		b=b.childNodes[5].innerHTML||b.childNodes[3].innerHTML // [5] if *edited post
-	}
+	if(a[7]) // comment post
+		a[5]+=' '+a[7];
+	b=document.querySelectorAll('[data-cid]')[0].getAttribute('data-author');
 	out(b+' '+a[5]+' ');
 }else if(a[1]=='www.youtube.com')
 	out(document.getElementsByClassName('yt-user-info')[0].textContent.trim()+' '+a[a.indexOf('v')+1]+' '+document.getElementById('eow-title').title);

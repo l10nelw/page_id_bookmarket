dbd2cc51424505aebfb2a321d9c935db0508f49b
Lionel Wong <lionelster@gmail.com>
2018-04-01 14:56:27 +0800

    Fix reddit part; beautify; rewrite metaprop

--- pageid.js
+++ pageid.js
@@ -1,35 +1,34 @@
-// without whitespace
-javascript:function getmeta(x){m=document.getElementsByTagName('meta');for(i=0;i<m.length;i++)if(m[i].getAttribute('property')==x)return m[i].getAttribute('content');return '';}function out(x){void prompt('',x);}a=location.href.split(/[?#]+/)[0].split(/\/+/);b=location.href.split(/[\/&=?#]+/);if(a[1]=='www.reddit.com'){if(a[7]){a[5]+=' '+a[7];c=document.title.split(' ')[0]}else c=document.querySelectorAll('[data-cid]')[0].getAttribute('data-author');out(c+' '+a[5]+' ');}else if(a[1]=='www.youtube.com')out(document.getElementsByClassName('yt-user-info')[0].textContent.trim()+' '+b[b.indexOf('v')+1]+' '+document.getElementById('eow-title').title);else if(a[1].slice(-11)=='.tumblr.com')out(a[1]+' '+a[3]);else if(a[1]=='www.facebook.com')out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML+' '+b.slice(2,6).join(' '));else if(a[1].slice(-15)=='.deviantart.com')out(a.slice(1,4).join(' '));else if(a[1]=='www.instagram.com'&&a[2]=='p'){out(window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.owner.username+' '+a[3]);location.href=getmeta('og:video')||getmeta('og:image')}else void(0)
-
-// with whitespace
 javascript:
-function getmeta(x){
-    m=document.getElementsByTagName('meta');
-    for(i=0;i<m.length;i++)
-		if(m[i].getAttribute('property')==x)
-			return m[i].getAttribute('content');
-    return '';
-}
-function out(x)void prompt('',x);
-a=location.href.split(/[?#]+/)[0].split(/\/+/);
-b=location.href.split(/[\/&=?#]+/);
-if(a[1]=='www.reddit.com'){
-	if(a[7]){ // comment post
-		a[5]+=' '+a[7];
-		c=document.title.split(' ')[0];
-	}else
-		c=document.querySelectorAll('[data-cid]')[0].getAttribute('data-author');
-	out(c+' '+a[5]+' ');
-}else if(a[1]=='www.youtube.com')
-	out(document.getElementsByClassName('yt-user-info')[0].textContent.trim()+' '+b[b.indexOf('v')+1]+' '+document.getElementById('eow-title').title);
-else if(a[1].slice(-11)=='.tumblr.com')
-	out(a[1]+' '+a[3]);
-else if(a[1]=='www.facebook.com')
-	out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML+' '+b.slice(2,6).join(' '));
-else if(a[1].slice(-15)=='.deviantart.com')
-	out(a.slice(1,4).join(' '));
-else if(a[1]=='www.instagram.com'&&a[2]=='p'){
-	out(window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.owner.username+' '+a[3]);
-	location.href=getmeta('og:video')||getmeta('og:image') // redirects to the media file url
-}else void(0);
+(_=>{
+    function metaprop(x) {
+        for (var m of document.querySelectorAll('meta')) {
+            if (m.getAttribute('property') == x) return m.content;
+        }
+        return '';
+    }
 
+    function out(x) {
+        void prompt('', x)
+    }
+        
+    var a = location.href.split(/[?#]+/)[0].split(/\/+/);
+    var b = location.href.split(/[\/&=?#]+/);
+    var a1 = a[1];
+    
+    if (a1 == 'www.reddit.com') {
+        a[7] // comment post
+        ? out(document.title.split(' ')[0] + ' ' + a[5] + ' ' + a[7] + ' ')
+        : out(document.querySelector('[data-author]').dataset.author + ' ' + a[5] + ' ');
+    } else if (a1 == 'www.youtube.com') {
+        out(document.querySelector('.yt-user-info').textContent.trim() + ' ' + b[b.indexOf('v') + 1] + ' ' + document.getElementById('eow-title').title);
+    } else if (a1.slice(-11) == '.tumblr.com') {
+        out(a1 + ' ' + a[3]);
+    } else if (a1 == 'www.facebook.com') {
+        out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML + ' ' + b.slice(2, 6).join(' '));
+    } else if (a1.slice(-15) == '.deviantart.com') {
+        out(a.slice(1, 4).join(' '));
+    } else if (a1 == 'www.instagram.com' && a[2] == 'p') {
+        out(window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.owner.username + ' ' + a[3]);
+        location.href = metaprop('og:video') || metaprop('og:image') // redirects to the media file url
+    } else void(0);
+})()

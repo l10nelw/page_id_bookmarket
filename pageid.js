f6ad5baf6f57f719c1cab6da8a3845b0a42dae77
Lionel Wong <lionelster@gmail.com>
2018-05-04 21:59:50 +0800

    Use template literals

--- pageid.js
+++ pageid.js
@@ -17,14 +17,14 @@ javascript:
     
     if (a1.endsWith('reddit.com')) {
         a[7] // comment post
-        ? out(document.title.split(' ')[0] + ' ' + a[5] + ' ' + a[7] + ' ')
-        : out(document.querySelector('[data-author]').dataset.author + ' ' + a[5] + ' ');
+        ? out(`${document.title.split(' ')[0]} ${a[5]} ${a[7]} `)
+        : out(`${document.querySelector('[data-author]').dataset.author} ${a[5]} `);
     } else if (a1 == 'www.youtube.com') {
-        out(document.querySelector('.yt-user-info').textContent.trim() + ' ' + b[b.indexOf('v') + 1] + ' ' + document.getElementById('eow-title').title);
+        out(`${document.querySelector('.yt-user-info').textContent.trim()} ${b[b.indexOf('v') + 1]} ${document.getElementById('eow-title').title}`);
     } else if (a1.slice(-11) == '.tumblr.com') {
-        out(a1 + ' ' + a[3]);
+        out(`${a1} ${a[3]}`);
     } else if (a1 == 'www.facebook.com') {
-        out(document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML + ' ' + b.slice(2, 6).join(' '));
+        out(`${document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML} ${b.slice(2, 6).join(' ')}`);
     } else if (a1.slice(-15) == '.deviantart.com') {
         out(a.slice(1, 4).join(' '));
     } else if (a1 == 'www.instagram.com' && a[2] == 'p') {

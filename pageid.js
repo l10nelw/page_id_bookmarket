af767a06db9bb3fa29662ee14ecf921a0b2309b4
Lionel Wong <lionelster@gmail.com>
2019-12-02 13:38:09 +0800

    Handle a photo series post

--- pageid.js
+++ pageid.js
@@ -1,20 +1,10 @@
 javascript:
 (_=>{
-    function metaprop(x) {
-        for (var m of document.querySelectorAll('meta')) {
-            if (m.getAttribute('property') == x) return m.content;
-        }
-        return '';
-    }
+    let out = x => void prompt('', x);
+    let a = location.href.split(/[?#]+/)[0].split(/\/+/);
+    let b = location.href.split(/[\/&=?#]+/);
+    let a1 = a[1];
 
-    function out(x) {
-        void prompt('', x)
-    }
-        
-    var a = location.href.split(/[?#]+/)[0].split(/\/+/);
-    var b = location.href.split(/[\/&=?#]+/);
-    var a1 = a[1];
-    
     if (a1.endsWith('reddit.com')) {
         a[7] // comment post
         ? out(`${document.title.split(' ')[0]} ${a[5]} ${a[7]} `)
@@ -29,6 +19,24 @@ javascript:
         out(a.slice(1, 4).join(' '));
     } else if (a1 == 'www.instagram.com' && a[2] == 'p') {
         out(window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.owner.username + ' ' + a[3]);
-        location.href = metaprop('og:video') || metaprop('og:image') // redirects to the media file url
+        let imgs = document.querySelectorAll('li img[srcset]');
+        let url;
+        if (imgs.length == 3) {
+            url = imgs[1].src;
+        } else if (imgs.length == 2) {
+            let i0 = imgs[0], i1 = imgs[1];
+            let top = !i0.closest('li').previousElementSibling;
+            let bot = !i1.closest('li').nextElementSibling;
+            if (top && bot) {
+                url = i0.src;
+                open(i1.src);
+            } else {
+                url = top ? i0.src : i1.src;
+            }
+        } else {
+            let meta = x => { for (let m of document.querySelectorAll('meta')) if (m.getAttribute('property') == x) return m.content };
+            url = meta('og:video') || meta('og:image');
+        }
+        location.href = url;
     } else void(0);
 })()

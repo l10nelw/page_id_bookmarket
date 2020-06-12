4f50e79b886ced2440289625ce33007f2b8ef107
Lionel Wong <lionelster@gmail.com>
2020-06-12 15:19:41 +0800

    Fix instagram case

--- pageid.js
+++ pageid.js
@@ -1,5 +1,8 @@
 javascript:
 (_=>{
+    let gebi = s => document.getElementById(s);
+    let qs = s => document.body.querySelector(s);
+    let qsa = s => document.body.querySelectorAll(s);
     let out = x => void prompt('', x);
     let a = location.href.split(/[?#]+/)[0].split(/\/+/);
     let b = location.href.split(/[\/&=?#]+/);
@@ -8,35 +11,23 @@ javascript:
     if (a1.endsWith('reddit.com')) {
         a[7] // comment post
         ? out(`${document.title.split(' ')[0]} ${a[5]} ${a[7]} `)
-        : out(`${document.querySelector('[data-author]').dataset.author} ${a[5]} `);
+        : out(`${qs('[data-author]').dataset.author} ${a[5]} `);
     } else if (a1 == 'www.youtube.com') {
-        out(`${document.querySelector('.yt-user-info').textContent.trim()} ${b[b.indexOf('v') + 1]} ${document.getElementById('eow-title').title}`);
+        out(`${qs('.yt-user-info').textContent.trim()} ${b[b.indexOf('v') + 1]} ${gebi('eow-title').title}`);
     } else if (a1.endsWith('tumblr.com')) {
         out(`${a1} ${a[3]}`);
     } else if (a1 == 'www.facebook.com') {
-        out(`${document.getElementById('fbPhotoPageAuthorName').firstChild.innerHTML} ${b.slice(2, 6).join(' ')}`);
+        out(`${gebi('fbPhotoPageAuthorName').firstChild.innerHTML} ${b.slice(2, 6).join(' ')}`);
     } else if (a1.endsWith('deviantart.com')) {
         out(a.slice(1, 4).join(' '));
     } else if (a1 == 'www.instagram.com' && a[2] == 'p') {
-        out(window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.owner.username + ' ' + a[3]);
-        let imgs = document.querySelectorAll('li img[srcset]');
-        let url;
-        if (imgs.length == 3) {
-            url = imgs[1].src;
-        } else if (imgs.length == 2) {
-            let i0 = imgs[0], i1 = imgs[1];
-            let top = !i0.closest('li').previousElementSibling;
-            let bot = !i1.closest('li').nextElementSibling;
-            if (top && bot) {
-                url = i0.src;
-                open(i1.src);
-            } else {
-                url = top ? i0.src : i1.src;
-            }
-        } else {
-            let meta = x => { for (let m of document.querySelectorAll('meta')) if (m.getAttribute('property') == x) return m.content };
-            url = meta('og:video') || meta('og:image');
-        }
+        let data = window.__additionalData;
+        for (let x in data) data = data[x].data.graphql.shortcode_media;
+        out(data.owner.username + ' ' + a[3]);
+        let media = qsa('li img[srcset], li video');
+        let url = media.length == 3 ? media[1].src :
+                  media.length == 2 ? media[qs('.coreSpriteRightChevron') ? 0 : 1].src :
+                  qs('img[srcset], video').src;
         location.href = url;
     } else void(0);
 })()
